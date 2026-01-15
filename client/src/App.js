c44a927bce5a827f1cdf088156485c2abc7d6f14
Cecilia Njoki Irungu <adelaideirungu@gmail.com>
2026-01-15 20:41:30 +0300

    Add routes for CreateEvent and CreateFeedback in React Router

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EventsList from "./components/EventsList";
 import EventDetail from "./components/EventDetail";
 import CreateEvent from "./components/CreateEvent";
+import CreateFeedback from "./components/CreateFeedback";
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/events">Events</Link> | <Link to="/create-event">Create Event</Link>
+        <Link to="/events">Events</Link> |{" "}
+        <Link to="/create-event">Create Event</Link> |{" "}
+        <Link to="/create-feedback">Leave Feedback</Link>
       </nav>
       <Routes>
         <Route path="/events" element={<EventsList />} />
         <Route path="/events/:id" element={<EventDetail />} />
         <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/create-feedback" element={<CreateFeedback />} />
       </Routes>
     </Router>
   );
